981854f1d0d278547c6938a467ebac63ce616587
AlexandreSout0 <soutoinf@gmail.com>
2022-12-31 14:16:24 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,30 +13,34 @@
 #define LED_BLUE GPIO_NUM_2
 #define V_OUT GPIO_NUM_13
 
-void Task_Undershoot(void *params);
+void Task_Undershoot(void *rams);
 void Task_ReadSerial(void *params);
 
 ledc_channel_config_t channel_config;
 ledc_timer_config_t timer_config ;
 
 String command;
+
 int interval = 5000;
 int tempo = 1000;
 int fall = 255;
-int fall2 = 255;
+
 
 
 void setup() 
 {
+
 
   Serial.begin(SERIAL_BAUDRATE);
   Serial.println("");
   Serial.println("Supply Undershoot Simulator");
   Serial.println("By: HIDROXID0");
 
+  vTaskDelay( 1000 / portTICK_PERIOD_MS);
   Serial.printf("Undershoot command: ");
   Serial.println("Interval@Time@Fall");
-
+  
+  Serial.printf("Repeat Interval: %d | Fall Time: %d  | Tension Fall: %d  \n", interval,tempo,fall);
   // -- Configuração dos pinos --
   //gpio_pad_select_gpio(GPIO_NUM_13);
   gpio_pad_select_gpio(GPIO_NUM_2);
@@ -63,16 +67,16 @@ void setup()
   channel_config.speed_mode = LEDC_LOW_SPEED_MODE;
   channel_config.channel = LEDC_CHANNEL_0;
   channel_config .timer_sel = LEDC_TIMER_0;
-  channel_config.duty = 0;
+  channel_config.duty = 255; // 255 pwm inicia com duty cycle 100% | 0 pwm inicia com duty cycle 0%
   channel_config.hpoint = 0;
   ledc_channel_config(&channel_config);
   //xTaskCreate(&Task_, "Descrição/nome da Task", Tamanho da Stack Memory 1024 = 4K de memória,Parametro da Função, Prioridade de 0 até uxPriority ,Task Random );
   //xTaskCreatePinnedToCore(&Task_, "Descrição/nome da Task", Tamanho da Stack Memory 1024 = 4K de memória,Parametro da Função, Prioridade de 0 até uxPriority ,Task Random, Núcleo de processamento);
   
-  xTaskCreatePinnedToCore(&Task_Undershoot, "Simulate Undershoot Supply", 2048, NULL, 1, NULL,1);
-  xTaskCreatePinnedToCore(&Task_ReadSerial,"Read Terminal Serial", 2048, NULL,1 , NULL, 2);
+  xTaskCreatePinnedToCore(&Task_Undershoot, "Simulate Undershoot Supply", 2048, NULL, 0, NULL,1);
+  xTaskCreatePinnedToCore(&Task_ReadSerial,"Read Terminal Serial", 2048, NULL,1 , NULL, 1);
 
-  ledc_fade_func_install(0);
+  ledc_fade_func_install(1);
 
 }
 
@@ -81,10 +85,10 @@ void loop()
 {
   
   
-  gpio_set_level(LED_BLUE, 1);
+   gpio_set_level(LED_BLUE, 0);
+   //Serial.println("loop teste");
+   vTaskDelay( 1000 / portTICK_PERIOD_MS); // Função de Delay do FreeRTOS, em tempo real. 1000ms = 1s
 
-  Serial.println("loop teste");
-  vTaskDelay( 1000 / portTICK_PERIOD_MS); // Função de Delay do FreeRTOS, em tempo real. 1000ms = 1s
 
 
 }
@@ -93,10 +97,13 @@ void Task_Undershoot(void * params)
 {
   while(true)
   {
+    
+    gpio_set_level(LED_BLUE, 1);
     Serial.println("UnderShoot");
-    ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0,139,1000, LEDC_FADE_WAIT_DONE);
-    ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0,255,1000, LEDC_FADE_WAIT_DONE);
-    vTaskDelay( 10000 / portTICK_PERIOD_MS); // Função de Delay do FreeRTOS, em tempo real. 1000ms = 1s
+    ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0,0,tempo, LEDC_FADE_WAIT_DONE);
+    ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0,fall,tempo, LEDC_FADE_WAIT_DONE);
+    
+    vTaskDelay( interval / portTICK_PERIOD_MS); // Função de Delay do FreeRTOS, em tempo real. 1000ms = 1s
 
   }
 }
@@ -105,30 +112,36 @@ void Task_Undershoot(void * params)
 
 void Task_ReadSerial (void *params)
 {
-  while(true)
+  int aux = 0;
+  while(1)
   {
     if (Serial.available() > 0)
     {
       command = Serial.readString();
-      Serial.println(command); 
+      Serial.println(command);
+      aux = 1;
     }
+
     //Interval@Time@Fall
     //10000@3000@255
-    String str1 = command.substring(0,4);
-    String str2 = command.substring(6,9);
-    String str3 = command.substring(11,13);
+    String str1 = command.substring(0,5);
+    String str2 = command.substring(6,10);
+    String str3 = command.substring(11,14);
 
-    interval = str1.toInt();
-    tempo = str2.toInt();
-    fall = str3.toInt();
+    if (aux == 1)
+    {
+        interval = str1.toInt();
+        tempo = str2.toInt();
+        fall = str3.toInt();
+        Serial.printf("Repeat Interval: %d  | ",interval);
+        Serial.printf("Fall Time: %d  | ",tempo);
+        Serial.printf("Tension Fall: %d \n",fall);
+        aux = 0;
+    }
 
-    Serial.printf("Repeat Interval: %d \n",interval);
-    Serial.printf("Fall Time: %d \n",tempo);
-    Serial.printf("Tension Fall: %d \n",fall);
+   vTaskDelay(1000 / portTICK_PERIOD_MS);
+   } 
 
-
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  } 
 }
 
 
